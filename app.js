a229bd34c6630adac4c7de5cb7ce4b7cd3338303
Lady Gaga <jessiezheqi.li@gmail.com>
2021-10-17 22:37:46 +0800

    Write file with short template

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-const commandLineArgs = process.argv.slice(2, process.argv.length);
-console.log(commandLineArgs);
+const fs = require("fs");
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+// template literal
+const generateMd = (userName, githubName) => {
+  return `# Project-name-placeholder
+## Description
+desc placeholder variable
+## How to install
+installment placeholder
+## Invite to collaborate
+An invitation text placeholder
+## License
+This application is open source.
+## Created by:
+  Name: ${userName}
+  GitHub: ![https://github.com/${githubName}]
+`;
+};
+//console.log(generateMd(name, github));
+
+//writeFile
+fs.writeFile("./README.md", generateMd(name, github), (err) => {
+  if (err) throw err;
+  console.log("File creating...Please check directory.");
+});
